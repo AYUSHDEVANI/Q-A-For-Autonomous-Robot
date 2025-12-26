75bbff7b916b345d2f0ae6d47767f44e5d1c220d
AYUSHDEVANI <ayushdevani0018@gmail.com>
2025-12-26 12:47:57 +0530

    Remove unused sentence-transformers import

--- app/services/chat_service.py
+++ app/services/chat_service.py
@@ -4,7 +4,7 @@ import logging
 import numpy as np
 from typing import TypedDict
 from cachetools import TTLCache
-from sentence_transformers import CrossEncoder
+# from sentence_transformers import CrossEncoder
 from langchain_groq import ChatGroq
 from langchain_core.prompts import ChatPromptTemplate
 from langgraph.graph import StateGraph, END
